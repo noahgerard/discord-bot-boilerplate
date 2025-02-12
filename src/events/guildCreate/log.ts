608d14c8b71340797aceff2f55d1318924fe8853
noahgerard <helix.dev.xyz@gmail.com>
2025-02-12 15:50:07 -0500

    feat: add guild entry on join

--- src/events/guildCreate/log.ts
+++ src/events/guildCreate/log.ts
@@ -1,7 +1,20 @@
 import { ButtonInteraction, Client, Guild } from "discord.js";
 import { logger } from "../../modules/logger.js";
+import { prisma } from "../../modules/prisma.js";
 
 export default async (guild: Guild, client: Client) => {	
 	logger.info(`Joined guild: ${guild.name} (${guild.id})`);
+
 	// TODO: Create guild entry in database
+	await prisma.guild.upsert({
+		where: {
+			id: guild.id,
+		},
+		update: {
+			id: guild.id,
+		},
+		create: {
+			id: guild.id,
+		},
+	});
 }
